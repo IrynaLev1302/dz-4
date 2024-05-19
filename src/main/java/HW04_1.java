94e10eb88b2484d153aae109d62d1e2277b1c48e
IrynaLev1302 <169169163+IrynaLev1302@users.noreply.github.com>
2024-05-19 19:14:48 +0300

    First commit

---
+++ src/main/java/HW04_1.java
@@ -0,0 +1,14 @@
+public class HW04_1 {
+
+    public static void main(String[] args) {
+        Number number = new Number();
+        number.displaynum();
+
+        System.out.println("~" + "~" + "~" + number.getNum() + "~" + "~" + "~");
+
+
+
+
+
+    }
+}
---
+++ src/main/java/Number.java
@@ -0,0 +1,15 @@
+public class Number {
+    private int num = 10;
+
+    public int getNum() {
+        return num;
+
+
+    }
+
+    public void displaynum() {
+        System.out.printf("-" + "-" + "-" + num + "-" + "-" + "-");
+
+
+    }
+}
